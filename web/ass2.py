ac60e1dcf92f5a3cf1fc9bfc8664e78b7075ee49
Tiantong  Li <glorialtt0118@gmail.com>
2019-05-07 00:12:44 +1000

    Tiantong-web page updates
    web page updates

--- web/ass2.py
+++ web/ass2.py
@@ -1,20 +1,52 @@
 from flask import Flask, render_template, url_for, jsonify, abort, request, make_response
-from flask_httpauth import HTTPBasicAuth
+# from flask_httpauth import HTTPBasicAuth
+import couchdb
+import requests
+import json
 app = Flask(__name__)
 
-auth = HTTPBasicAuth()
+server_url = "http://127.0.0.1:5984/"
 
-@auth.get_password
-def get_password(username):
-    if username == 'admin':
-        return 'admin'
-    return None
 
-@auth.error_handler
-def unauthorized():
-    return make_response(jsonify( { 'error': 'Unauthorized access' } ), 403)
-    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
-    
+def getDataFromCouchDB(data):
+    # this function will convert a list of dictionaries obtained from couchDB into a dictionary
+    # which contains all required information for analysis
+	dic = {}
+
+    # iterate all documents
+    for i in data:
+        try:
+            temp = {}
+            temp["twitter_result"] = i['twitter_result']
+            temp["obesity"] = i['obesity']
+            temp["heart_disease"] = i['heart_disease']
+            dic[i['city']] = temp
+		except:
+			continue
+	return dic
+
+try:
+    couch = couchdb.Server(server_url)
+except:
+    print "Cannot find CouchDB Server ... Exiting\n"
+    print "----_Stack Trace_-----\n"
+    raise
+
+# couchdb username and password
+couch.resource.credentials = ('admin', 'admin')
+
+# locate to certain database
+db = couch['data_analysis']
+
+# all docs including views
+rows = db.view('_all_docs', include_docs=True)
+
+# transfer couchdb object to a list, each list contains a dictionary
+raw_data = [row['doc'] for row in rows]
+
+# collect all required data into one dictionary
+data = getDataFromCouchDB(raw_data)
+
 @app.errorhandler(400)
 def bad_request(error):
     return make_response(jsonify( { 'error': 'Bad request' } ), 400)
@@ -25,10 +57,17 @@ def not_found(error):
 
 # so can access home page though two paths
 @app.route("/")
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
 @app.route("/home")
 def home():
     return render_template('home.html')
 
+@app.route("/twitter")
+def twitter():
+    return render_template('twitter.html', data = json.dumps(data))
 
 @app.route("/about")
 def about():
